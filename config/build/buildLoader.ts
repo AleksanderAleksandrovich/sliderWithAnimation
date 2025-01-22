0f402426cb300abb8b248c3715ce5ef6e9c29043
AleksanderAleksandrovich <aleksveles228@gmail.com>
2025-01-22 21:41:08 +0300

    add styleLoader and processing scss

--- config/build/buildLoader.ts
+++ config/build/buildLoader.ts
@@ -1,12 +1,33 @@
 import { RuleSetRule } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
 
-
-export const buildLoaders = (): RuleSetRule[] => {
+export const buildLoaders = ({ isDev }: BuildOptions): RuleSetRule[] => {
   const tsLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
 
-  return [tsLoader];
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            namedExport: false,
+            auto: (path: string) => Boolean(path.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  return [scssLoader, tsLoader];
 };
