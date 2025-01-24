f8c469668802ca78a49d94d3b80f121d21f027fc
AleksanderAleksandrovich <aleksveles228@gmail.com>
2025-01-24 19:59:41 +0300

    add animation to Slider

--- src/widgets/SliderForCards/ui/Sidebar/Slider.tsx
+++ src/widgets/SliderForCards/ui/Sidebar/Slider.tsx
@@ -1,31 +1,70 @@
 import "swiper/scss";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperClass, SwiperRef, SwiperSlide } from "swiper/react";
+import gsap from "gsap";
 
 import classes from "./Slider.module.scss";
 import { ButtonControl } from "../ButtonControl/ButtonControl";
 import { Card } from "../Card/Card";
+import { Fact } from "shared/data/type";
+import { classNames } from "shared/lib/classNames/classNames";
 
-import { data } from "shared/data/data";
+type SliderForCardsProps = {
+  facts: Fact[];
+  className?: string;
+};
 
-export const SliderForCards = () => {
+export const SliderForCards = ({ facts, className }: SliderForCardsProps) => {
   const swiperRef = useRef<SwiperRef>(null);
+  const wrapperRef = useRef(null);
   const [isBeginning, setIsBeginning] = useState(true);
   const [isEnd, setIsEnd] = useState(false);
+  const [currentFacts, setCurrentFacts] = useState(facts);
+
+  useEffect(() => {
+    let fadeOutAnimation: ReturnType<typeof gsap.to>;
+
+    if (facts !== currentFacts) {
+      fadeOutAnimation = gsap.to(wrapperRef.current, {
+        opacity: 0,
+        duration: 0.3,
+        onComplete: () => {
+          setCurrentFacts(facts);
+
+          gsap.fromTo(
+            wrapperRef.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 0.3 }
+          );
+        },
+      });
+    }
+
+    return () => {
+      if (fadeOutAnimation) {
+        fadeOutAnimation.kill();
+      }
+    };
+  }, [facts]);
 
   const prevHandler = () => {
-    swiperRef.current.swiper.slidePrev();
+    swiperRef.current?.swiper.slidePrev();
   };
+
   const nextHandler = () => {
-    swiperRef.current.swiper.slideNext();
+    swiperRef.current?.swiper.slideNext();
   };
 
   const onSlideChange = (slide: SwiperClass) => {
     setIsBeginning(slide.isBeginning);
     setIsEnd(slide.isEnd);
   };
+
   return (
-    <div className={classes.sliderWrapper}>
+    <div
+      className={classNames(classes.sliderWrapper, {}, [className])}
+      ref={wrapperRef}
+    >
       {!isBeginning && (
         <ButtonControl
           direction="left"
@@ -46,8 +85,8 @@ export const SliderForCards = () => {
         ref={swiperRef}
         onSlideChange={onSlideChange}
       >
-        {data.map((el) => (
-          <SwiperSlide key={el.title}>
+        {currentFacts.map((el) => (
+          <SwiperSlide key={el.id}>
             <Card fact={el} />
           </SwiperSlide>
         ))}
