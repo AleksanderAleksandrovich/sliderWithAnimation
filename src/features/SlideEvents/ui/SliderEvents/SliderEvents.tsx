d7dee59076117ddf9c2ba673dbee62cf1d47b0fe
AleksanderAleksandrovich <aleksveles228@gmail.com>
2025-01-24 20:06:42 +0300

    changes SliderEvents

--- src/features/SlideEvents/ui/SliderEvents/SliderEvents.tsx
+++ src/features/SlideEvents/ui/SliderEvents/SliderEvents.tsx
@@ -1,20 +1,57 @@
-import { SliderForCards } from "widgets/SliderForCards";
-import classes from "./SliderEvents.module.scss";
-import { classNames } from "shared/lib/classNames/classNames";
+import { useDispatch, useSelector } from "react-redux";
 import { SliderEventsTitle } from "../SliderEventsTitle/SliderEventsTitle";
 import { SliderEventsDate } from "../SliderEventsDate/SliderEventsDate";
+import { getDateEventsState } from "../../model/selectors/getDateEventsState";
+import { dateEventsActions } from "../../model/slice/dataEventsSlice";
+import { SliderEventsControl } from "../SliderEventsControl/SliderEventsControl";
+import classes from "./SliderEvents.module.scss";
 
+import { SliderForCards } from "widgets/SliderForCards";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Text, TextSize } from "shared/ui/Text/Text";
 type SliderEventsProps = {};
 
 export const SliderEvents = ({}: SliderEventsProps) => {
+  const { dateEvents, current } = useSelector(getDateEventsState);
+  const dispatch = useDispatch();
+
+  const isEnd = current === dateEvents.length - 1;
+  const isStart = current === 0;
+
+  const handleNext = () => {
+    dispatch(dateEventsActions.nextDateInterval());
+  };
+  const handlePrev = () => {
+    dispatch(dateEventsActions.prevDateInterval());
+  };
+
   return (
     <div className={classNames(classes.sliderEvents, {}, [])}>
       <div className={classes.verticalLine} />
       <div className={classes.horizontalLine} />
       <div className={classes.circle} />
       <SliderEventsTitle value="Исторические даты" />
-      {/* <SliderEventsDate beginDate="2015" endDate="2022" className={classes.date} /> */}
-      <SliderForCards />
+      <SliderEventsDate
+        beginDate={dateEvents[current].dateStart}
+        endDate={dateEvents[current].dateEnd}
+        className={classes.date}
+      />
+      <div className={classes.wrapperBlockControl}>
+        <Text
+          size={TextSize.S}
+          value={`0${current + 1}/0${dateEvents.length}`}
+        />
+
+        <div className={classes.wrapperController}>
+          <SliderEventsControl
+            direction="left"
+            onClick={handlePrev}
+            disabled={isStart}
+          />
+          <SliderEventsControl onClick={handleNext} disabled={isEnd} />
+        </div>
+      </div>
+      <SliderForCards facts={dateEvents[current].facts} className={classes.sliderCards}/>
     </div>
   );
 };
