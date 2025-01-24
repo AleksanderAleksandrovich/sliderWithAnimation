c3eaa5e15a1491aa124540f26667d766851a845b
AleksanderAleksandrovich <aleksveles228@gmail.com>
2025-01-24 19:56:44 +0300

    add TextSize to component Text

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -1,11 +1,21 @@
 import classes from "./Text.module.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 
+export enum TextSize {
+  S = "sizeS",
+  M = "sizeM",
+}
+
 type TextProps = {
   className?: string;
+  size?: TextSize;
   value: string;
 };
 
-export const Text = ({ className, value }: TextProps) => {
-  return <p className={classNames(classes.text, {}, [className])}>{value}</p>;
+export const Text = ({ className, value, size = TextSize.M }: TextProps) => {
+  return (
+    <p className={classNames("", {}, [className, classes[size]])}>
+      {value}
+    </p>
+  );
 };
