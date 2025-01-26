1b887b29ca4a4e73143285b69105e81dcf84b72f
AleksanderAleksandrovich <aleksveles228@gmail.com>
2025-01-26 22:59:07 +0300

    add SliderEvents mobile version

--- src/features/SliderEvents/ui/SliderEvents/SliderEvents.tsx
+++ src/features/SliderEvents/ui/SliderEvents/SliderEvents.tsx
@@ -5,10 +5,13 @@ import { SliderEventsControl } from "../SliderEventsControl/SliderEventsControl"
 import { CircleButtons } from "../CircleButtons/CircleButtons";
 import { DateEvent } from "../../model/types/dateEventsSchema";
 import classes from "./SliderEvents.module.scss";
+import { TitleEvent } from "../TitleEvent/TitleEvent";
 
 import { SliderForCards } from "widgets/SliderForCards";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Text, TextSize } from "shared/ui/Text/Text";
+import { useWindowWidth } from "shared/lib/hooks/useWindowWidth";
+import { Button, ButtonSize, ButtonTheme } from "shared/ui/Button/Button";
 
 type SliderEventsProps = {
   events: DateEvent[];
@@ -27,10 +30,13 @@ export const SliderEvents = ({
   handleSetEvent,
   title,
 }: SliderEventsProps) => {
+  const widthWindow = useWindowWidth();
   const isEnd = current === events.length - 1;
   const isStart = current === 0;
 
-  return (
+  const isDesktop = widthWindow > 500;
+
+  const desktopVersion = (
     <div className={classNames(classes.sliderEvents, {}, [])}>
       <div className={classes.verticalLine} />
       <div className={classes.horizontalLine} />
@@ -66,4 +72,62 @@ export const SliderEvents = ({
       />
     </div>
   );
+
+  const mobileVersion = (
+    <div className={classNames(classes.sliderEvents, {}, [])}>
+      <SliderEventsTitle value={title} isDesktop={isDesktop} />
+      <SliderEventsDate
+        beginDate={events[current].dateStart}
+        endDate={events[current].dateEnd}
+        className={classes.date}
+      />
+
+      <TitleEvent
+        titleEvent={events[current].title}
+        className={classes.titleEvent}
+        isDesktop={false}
+      />
+
+      <div className={classes.horizontalLineMobile} />
+
+      <SliderForCards
+        facts={events[current].facts}
+        className={classes.sliderCards}
+        isDesktop={isDesktop}
+      />
+      <div className={classes.wrapperBlockControl}>
+        <Text size={TextSize.S} value={`0${current + 1}/0${events.length}`} />
+
+        <div className={classes.wrapperController}>
+          <SliderEventsControl
+            direction="left"
+            onClick={handlePrev}
+            disabled={isStart}
+            isDesktop={isDesktop}
+          />
+          <SliderEventsControl
+            onClick={handleNext}
+            disabled={isEnd}
+            isDesktop={isDesktop}
+          />
+        </div>
+      </div>
+      <div className={classes.paginationWrapper}>
+        {events.map(({ dateEnd }, index) => (
+          <Button
+            key={dateEnd}
+            circle
+            selected={index === current}
+            onClick={() => handleSetEvent(index)}
+            theme={ButtonTheme.WITHOUTBORDER}
+            size={ButtonSize.SMOBILE}
+          />
+        ))}
+      </div>
+    </div>
+  );
+
+  if (isDesktop) return desktopVersion;
+
+  return mobileVersion;
 };
